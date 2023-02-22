8b1a4a12e7183aa95b692b43feb3cd3e70c4850e
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-22 13:51:51 -0300

    created children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template', loadChildren: () => import('./template/template.module').then(t => t.TemplateModule) },
+  { path: 'reactive', loadChildren: () => import('./reactive/reactive.module').then(r => r.ReactiveModule) },
+];
 
 @NgModule({
   imports: [
