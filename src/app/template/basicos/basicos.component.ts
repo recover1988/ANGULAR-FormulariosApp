cb6c8aa94fd75b1bdd1a321f93a1caaeb2258a9c
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-22 17:07:52 -0300

    validacion visual

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -15,9 +15,12 @@ export class BasicosComponent {
   nombreValido(): boolean {
     return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto'].touched;
   };
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched && this.miFormulario?.controls['precio'].value < 0;
+  };
 
   // guardar(miFormulario: NgForm) {
   guardar() {
-    console.log(this.miFormulario.value)
+    console.log(this.miFormulario.value, 'asd')
   };
 }
