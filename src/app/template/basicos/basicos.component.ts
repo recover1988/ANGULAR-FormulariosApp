1609a37f5768c53b8bb2404ebd0e36ce1b80052e
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-22 16:32:28 -0300

    viewchild

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,7 +8,16 @@ import { NgForm } from '@angular/forms';
   ]
 })
 export class BasicosComponent {
-  guardar(miFormulario: NgForm) {
-    console.log(miFormulario.value)
-  }
+
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto'].touched;
+  };
+
+  // guardar(miFormulario: NgForm) {
+  guardar() {
+    console.log(this.miFormulario.value)
+  };
 }
