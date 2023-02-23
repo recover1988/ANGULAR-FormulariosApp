510e2aca3eaebc7fe4191a1d073e45fa338dbb5d
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-23 13:43:54 -0300

    formBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,12 +9,47 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class BasicosComponent {
 
-miFormulario:FormGroup= new FormGroup({
-  'nombre': new FormControl('RTX 4800ti')
-})
+  miFormulario: FormGroup = this.fb.group({
+    'nombre': ['RTX 4800ti'],
+    'precio': [1555],
+    'existencias': [12],
+  })
 
 
 
 
-  constructor() { };
+  constructor(private fb: FormBuilder) { };
 }
+
+/*
+  miFormulario: FormGroup = this.fb.group({
+    'nombre': ['RTX 4800ti', |VALIDACIONES|, |VALIDACIONES_ASINCRONAS|],
+    'precio': [1555],
+    'existencias': [12],
+  })
+*/
+
+
+/*
+  miFormulario: FormGroup = new FormGroup({
+    'nombre': new FormControl('RTX 4800ti'),
+    'precio': new FormControl(1550),
+    'existencias': new FormControl(12),
+  })
+*/
+
+/*
+miFormulario:FormGroup= new FormGroup({
+  'nombre'      : new FormControl('RTX 4800ti'),
+  'precio'      : new FormControl(1550),
+  'existencias' : new FormControl(12),
+  'existencias' : new FormControl(12),
+  'existencias' : new FormControl(12),
+  'existencias' : new FormArray([
+    new FormControl(12),
+    new FormControl(12),
+    new FormControl(12),
+    new FormControl(12),
+  ]),
+})
+*/
