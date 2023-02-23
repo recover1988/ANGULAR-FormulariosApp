5b6c4a971da6cef171f68442d3b7bf5b759a0792
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-23 15:12:56 -0300

    tarea form builder

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class DinamicosComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]]
+  })
 
+
+  campoEsValido(campo: string) {
+    return this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+  }
+
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value)
+  }
+  constructor(private fb: FormBuilder) { };
 }
