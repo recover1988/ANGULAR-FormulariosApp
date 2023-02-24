71ac8c14eab37703eba20c3546ce26c5cb487a65
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-23 23:21:50 -0300

    validar contraseñas iguales

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -11,13 +11,14 @@ import { ValidatorService } from '../../../shared/validator/validator.service';
 })
 export class RegistroComponent implements OnInit {
 
-  //TODO temporal
-
-
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)]],
     email: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
     username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  }, {
+    validators: [this.vs.camposIguales('password', 'password2')]
   })
 
   campoNoValido(campo: string) {
@@ -29,7 +30,6 @@ export class RegistroComponent implements OnInit {
 
     this.miFormulario.markAllAsTouched()
   }
-
 
 
 
--- src/app/shared/validator/validator.service.ts
+++ src/app/shared/validator/validator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormControl, ValidationErrors } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +17,21 @@ export class ValidatorService {
       }
     }
     return null
+  }
+  camposIguales(campo1: string, campo2: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+
+
+      const pass1 = formGroup.get(campo1)?.value;
+      const pass2 = formGroup.get(campo2)?.value;
+      if (pass1 !== pass2) {
+        // asignar errores a un campo
+        formGroup.get(campo2)?.setErrors({ noIguales: true })
+        return { noIguales: true }
+      }
+      formGroup.get(campo2)?.setErrors(null)
+      return null
+    }
   }
 
 
