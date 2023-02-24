ae961432430885775a7bdee3fd955388d65a71fb
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-23 22:24:52 -0300

    validacion personalizada

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,6 +7,52 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
 
+  //TODO temporal
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  noPuedeSerStrider(control: FormControl) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if (valor === 'strider') {
+      return {
+        noStrider: true
+      }
+    }
+    return null
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+    username: ['', [Validators.required, this.noPuedeSerStrider]],
+  })
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value)
+
+    this.miFormulario.markAllAsTouched()
+  }
+
+
+
+
+
+
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Eric Denis',
+      email: 'test1@test.com',
+      username: 'ericdenis'
+    })
+  }
+
+
+  constructor(private fb: FormBuilder) { }
 }
